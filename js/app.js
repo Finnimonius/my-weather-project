97c25ab867190a81d93a105c5c9ee6b09c284ccf
Aleksandr Zykov <Finnimonius97@gmail.com>
2025-08-18 13:30:39 +0400

    intermediate logic to API data

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { getCoordinates } from "./geoService.js";
+import { getCoordinates, getCityName } from "./geoService.js";
 
 const WEATHER_CODES = [
     { codes: [0], description: "Чистое небо" },
@@ -16,22 +16,102 @@ const WEATHER_CODES = [
     { codes: [96, 99], description: "Гроза с небольшим и сильным градом" }
 ];
 
-const coords = await getCoordinates("443031");
+const app = document.getElementById("app")
+const temperatureSpan = document.getElementById("weather-temperature");
+const citySpan = document.getElementById("weather-city");
+const dateSpan = document.getElementById("weather-date");
+const searchInput = document.getElementById("search-input");
+const precipitation = document.getElementById("precipitation");
+const tempMax = document.getElementById("temp-max");
+const tempMin = document.getElementById("temp-min");
+const humidity = document.getElementById("humidity");
+const cloudy = document.getElementById("cloudy");
+const wind = document.getElementById("wind");
+
+const checkWeatherCode = (code) => {
+    try {
+        const weatherDescr = WEATHER_CODES.find(weatherCodeObj => {
+            return code === weatherCodeObj.codes.find(item => item === code)
+        }).description
+        return weatherDescr
+    } catch (error) {
+        console.error("Такого кода описания погоды не существует", error)
+        return " "
+    }
+}
+
+const getMaxMinTemp = (daylyTemp) => {
+    const daylyArr = [...daylyTemp].sort((a, b) => a - b)
+    return {
+        min: daylyArr[0],
+        max: daylyArr[daylyArr.length - 1]
+    }
+}
+
+const displayWeather = (weather, cityName) => {
+    const { current_weather, hourly } = weather
+    temperatureSpan.textContent = `${current_weather.temperature}°`
+    citySpan.textContent = cityName
+    precipitation.textContent = checkWeatherCode(current_weather.weathercode);
+
+    const { min, max } = getMaxMinTemp(hourly.temperature_2m);
+    tempMax.textContent = `${max}°`;
+    tempMin.textContent = `${min}°`;
+    humidity.textContent = `${hourly.relative_humidity_2m[0]}%`;
+    cloudy.textContent = `${hourly.cloudcover[0]}%`;
+    wind.textContent = `${(Number(current_weather.windspeed) * 1000) / 3600} м/c`
+}
+
+async function fetchUserGeo() {
+    try {
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        })
+        const { latitude: lat, longitude: lon } = position.coords;
+
+        return [lat, lon]
+    } catch (error) {
+        console.error("Ошибка геолокации пользователя:", error)
+
+        return ['55.6255780', '37.6063916']
+    }
+}
 
 async function getWeather(coordinates) {
     try {
-        let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates[0]}&longitude=${coordinates[1]}&hourly=temperature_2m,precipitation,weathercode,windspeed_10m&current_weather=true&timezone=auto&forecast_days=1`);
+        let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates[0]}&longitude=${coordinates[1]}&hourly=temperature_2m,precipitation,weathercode,windspeed_10m,relative_humidity_2m,cloudcover&current_weather=true&timezone=auto&forecast_days=1`);
         let data = await response.json()
 
         if (!response.ok) {
             throw new Error(`API error: ${response.status}`)
         }
 
+        console.log(data)
         return data
     } catch (error) {
         console.error(error)
     }
 }
 
-const weatherObj = await getWeather(coords)
-console.log(weatherObj)
+async function initWeather() {
+    try {
+        const userGeo = await fetchUserGeo()
+        const city = await getCityName(userGeo)
+        const weatherLoad = await getWeather(userGeo)
+        displayWeather(weatherLoad, city)
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+searchInput.addEventListener("change", async () => {
+    const coords = await getCoordinates(searchInput.value)
+    const city = await getCityName(coords)
+    const weather = await getWeather(coords)
+    displayWeather(weather, city)
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    initWeather()
+})
--- js/geoService.js
+++ js/geoService.js
@@ -2,13 +2,23 @@ export async function getCoordinates(address) {
     try {
         let response = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json`)
         let data = await response.json()
-        console.log(data)
-        
-        if(!data || data.length === 0) {
+
+        if (!data || data.length === 0) {
             throw new Error("Адрес не найден")
         }
 
         return [data[0].lat, data[0].lon]
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export async function getCityName(coords) {
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${coords[0]}&lon=${coords[1]}&format=json`);
+        const data = await response.json();
+
+        return data.address.city
     } catch (error) {
         console.error(error)
     }
